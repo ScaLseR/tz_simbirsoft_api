3265d0ea0d89c2c80097cd0992a329e212786f28
scalser <scalserstv@yandex.ru>
2022-07-25 18:42:17 +0300

    added 2-nd assert

--- api_connector.py
+++ api_connector.py
@@ -1,11 +1,6 @@
 """connector for http server"""
 from json import loads
-
 from requests import request
-
-
-
-
 
 _UPLOAD_END_POINT = '/api/upload'
 _DELETE_END_POINT = '/api/delete'
@@ -32,8 +27,6 @@ class ConnectorHttp:
     def __init__(self, http_url):
         self._upload = request_preparation(http_url, _UPLOAD_END_POINT)
         self._delete = request_preparation(http_url, _DELETE_END_POINT)
-
-
 
     def download_without_param(self):
         """download file without parameters"""
--- api_test.py
+++ api_test.py
@@ -1,11 +1,14 @@
 from requests import request
 import pytest
+import json
+
 
 URL = 'https://cloud-api.yandex.net/v1/disk/resources'
 TOKEN = 'OAuth AQAAAABjT3cJAADLW8UcQbr98UJct8YGQ9RzV1w'
 params = dict(path='tz_simbirsoft')
 headers = dict(accept='application/json', authorization='OAuth AQAAAABjT3cJAADLW8UcQbr98UJct8YGQ9RzV1w')
-resp_body = {"href":"https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2Ftz_simbirsoft","method":"GET","templated":false}
+resp_body = dict(href="https://cloud-api.yandex.net/v1/disk/resources?path=disk%3A%2Ftz_simbirsoft", method="GET",
+                 templated=False)
 
 
 class TestApi:
@@ -14,7 +17,8 @@ class TestApi:
     def test_create_folder(self):
         res = request(method='put', url=URL, headers=headers, params=params)
         print('status code', res.status_code)
-        print('telo otveta', res.content.decode('utf-8'))
+        print('telo otveta', json.loads(res.content.decode('utf-8')))
         assert str(res.status_code) == '201'
+        assert json.loads(res.content.decode('utf-8')) == resp_body
 
 
